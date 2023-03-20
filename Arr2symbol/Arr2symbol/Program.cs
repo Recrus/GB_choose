d64a109350a8d2f390fbd1c44d192c2470551f5a
Recrus <dan.plishkin@gmail.com>
2023-03-20 22:18:15 +0300

    first commit and solution

---
+++ Arr2symbol/Arr2symbol/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] items = {"1", "22", "33", "444", "-a"};
+
+void Main(string[] arr)
+{
+        //first solution
+        string[] res = { };
+        for (int i = 0; i < arr.Length; i++)
+        {
+                if (arr[i].Length == 2)
+                {
+                        res = res.Append(arr[i]).ToArray();
+                }
+        }
+        Console.WriteLine("[{0}]", string.Join(", ", res));
+        
+        //second solution
+        // arr = arr.Where(val => val.Length == 2).ToArray();
+        // Console.WriteLine("[{0}]", string.Join(", ", arr));
+};
+
+Main(items);
